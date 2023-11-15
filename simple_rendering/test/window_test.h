0a20c576e331b201ed8175cc355f60e37c63bef2
Zch <zhongch2003@gmail.com>
2023-11-15 10:29:29 +0800

    [Testing] Revised window test

--- simple_rendering/test/window_test.h
+++ simple_rendering/test/window_test.h
@@ -1,23 +1,53 @@
 #include <gtest/gtest.h>
 #include "../include/window.h"
 
-TEST(WindowSuite, CreateWindow) {
-    if (!glfwInit()) {
-        FAIL() << "Failed to initialize GLFW";
+class WindowSuite : public ::testing::Test {
+protected:
+    void SetUp() override {
+        if (skipAll) {
+            GTEST_SKIP();
+        }
+
+        SetUpGlfw();
     }
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    void TearDown() override {
+        if (skipAll) {
+            return;
+        }
 
+        glfwTerminate();
+    }
+
+    void SetUpGlfw() {
+        if (!glfwInit()) {
+            skipAll = true;
+            FAIL() << "Failed to initialize GLFW";
+        }
+
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    }
+
+    void SetUpGlad() {
+        if (!gladLoadGL(glfwGetProcAddress)) {
+            skipAll = true;
+            FAIL() << "Failed to initialize GLAD";
+        }
+    }
+
+    static bool skipAll;
+};
+
+bool WindowSuite::skipAll = false;
+
+TEST_F(WindowSuite, CreateWindow) {
     Window window(800, 600, "Test Window");
     window.makeContextCurrent();
 
-    if (!gladLoadGL(glfwGetProcAddress)) {
-        FAIL() << "Failed to initialize GLAD";
-    }
+    SetUpGlad();
 
     ASSERT_EQ(glfwGetCurrentContext(), window.getGLFWWindow());
 
     glfwSetWindowShouldClose(window.getGLFWWindow(), true);
-    glfwTerminate();
 }
